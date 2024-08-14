666f2b1f8b47c950eaf3e1aecb75d0fc8ddbbc66
Jove Zhong <jove@timeplus.io>
2024-08-14 16:23:53 -0700

    add sample code for pandas, extract db account

--- example/streaming_query/car.py
+++ example/streaming_query/car.py
@@ -10,9 +10,10 @@ import time
 
 from proton_driver import connect
 
+account='default:'
 
 def create_stream():
-    with connect("proton://default:@localhost:8463/default") as conn:
+    with connect(f"proton://{account}@localhost:8463/default") as conn:
         with conn.cursor() as cursor:
             cursor.execute("drop stream if exists cars")
             cursor.execute("create stream if not exists car(id int64, speed float64)")
@@ -25,7 +26,7 @@ def write_data(car_num: int):
         data = [(random.randint(0, car_num - 1), random.random() * 20 + 50,
                  car_begin_date
                  + datetime.timedelta(milliseconds=i * 100)) for i in range(300000)]
-        with connect("proton://default:@localhost:8463/default") as conn:
+        with connect(f"proton://{account}@localhost:8463/default") as conn:
             with conn.cursor() as cursor:
                 cursor.executemany("insert into car (id, speed, _tp_time) values", data)
                 print(f"row count: {cursor.rowcount}")
@@ -33,8 +34,8 @@ def write_data(car_num: int):
 
 
 def query(conn):
-    query_sql = """select id, avg(speed), window_start, window_end 
-                from session(car, 1h, [speed >= 60, speed < 60)) 
+    query_sql = """select id, avg(speed), window_start, window_end
+                from session(car, 1h, [speed >= 60, speed < 60))
                 group by id, window_start, window_end"""
     cursor = conn.cursor()
     cursor.set_stream_results(stream_results=True, max_row_buffer=100)
@@ -49,7 +50,7 @@ def fetch_result(cursor):
 
 def main():
     create_stream()
-    conn = connect("proton://default:@localhost:8463/default")
+    conn = connect(f"proton://{account}@localhost:8463/default")
     cursor = query(conn)
     threading.Thread(target=write_data, args=[7]).start()
     fetch_result(cursor)
